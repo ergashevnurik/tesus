7039bfa2dab89a8a40272c887c09f8cce1f799a0
Nurmuhammad Ergashev <ergashev.nurik01@mail.ru>
2021-03-18 22:33:54 +0500

    JS BUG in OnScroll function()

--- js/scroll.js
+++ js/scroll.js
@@ -7,11 +7,11 @@ window.onscroll = function() {
 }
 
 function revealOnScroll() {
-      if (document.body.scrollTop > 100 || document.documentElement.scrollTop < 100) {
-        navbar.style.padding = "0";
-        navbar.style.borderBottom = "none";
-      } else {
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 0) {
         navbar.style.padding = "5px";
         navbar.style.borderBottom = "3px solid #007bff";
+      } else {
+        navbar.style.padding = "0";
+        navbar.style.borderBottom = "none";
       }
 }
