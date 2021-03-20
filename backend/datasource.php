f35260cc8885c907bf72317fcb4c04e1343e4d0d
Nurmuhammad Ergashev <ergashev.nurik01@mail.ru>
2021-03-20 15:09:28 +0500

    DB

--- backend/datasource.php
+++ backend/datasource.php
@@ -9,25 +9,26 @@ $email = $_POST['email'];
 $serverNameLocalhost = "localhost";
 $username = "root";
 $password = "";
+$dbName = "contact-request";
 
-$conn = new mysqli($serverNameLocalhost, $username, $password);
+$conn = new mysqli($serverNameLocalhost, $username, $password, $dbName);
 
 if ($conn->connect_error) {
     die("Connection Failure: " . $conn->connect_error);
 } else {
+    // $sqlCreateTable = "CREATE DATABASE contactRequest"; // Create Table SQL 
+    // if ($conn->query($sqlCreateTable) === TRUE) {
+    //     echo "DATABASE with name contactRequest";
+    // } else {
+    //     echo "Error: " . $conn->error;
+    // }
+
     $sqlInsertToTable = "insert into contactRequest(firstName, phone, email) values(?, ?, ?)"; // Insert into Database the Users Requests
     $stmt = $conn->prepare($sqlInsertToTable);
     $stmt->bind_param("sss", $firstName, $phone, $email);
     $stmt->execute();
     echo "Was Successfully Inserted...";
     $stmt->close();
-}
-
-$sqlCreateTable = "CREATE DATABASE contactRequest"; // Create Table SQL 
-if ($conn->query($sqlCreateTable) === TRUE) {
-    echo "DATABASE with name contactRequest";
-} else {
-    echo "Error: " . $conn->error;
 }
 
 $conn->close();
