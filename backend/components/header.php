3e6ebd5277f54cd61a0407cadca535483f3c3817
Nurmuhammad Ergashev <ergashev.nurik01@mail.ru>
2021-03-21 09:40:36 +0500

    PHP Initialize

---
+++ backend/components/header.php
@@ -0,0 +1,45 @@
+<?php 
+
+// Navbar Goes Here
+echo '<nav class="navbar navbar-expand-md fixed-top">
+<div class="container">
+    <!-- Brand -->
+    <a class="navbar-brand" href="#">
+        <img src="./img/TESUS_KP.png" alt="">
+    </a>
+
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <i class="bi bi-list"></i>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#hero">Главная Страница</a>
+                <div class="nav-line"></div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#about">О Нас</a>
+                <div class="nav-line"></div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#services">Услуни</a>
+                <div class="nav-line"></div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#blog">Блог</a>
+                <div class="nav-line"></div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#contacts">Контакты</a>
+                <div class="nav-line"></div>
+            </li>
+        </ul>
+    </div>
+</div>
+</nav>';
+//Navbar Ends Here
+
+?>
