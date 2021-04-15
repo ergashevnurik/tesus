1c357e91914999a5030da2fd2544056637841da5
Nurmuhammad Ergashev <ergashev.nurik01@mail.ru>
2021-04-15 16:57:28 +0500

    a

--- js/scroll.js
+++ js/scroll.js
@@ -10,8 +10,10 @@ function revealOnScroll() {
       if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 0) {
         navbar.style.borderBottom = "5px solid #007bff";
         navbar.style.background = "#fff";
+        navbar.style.color = "#000";
       } else {
         navbar.style.borderBottom = "none";
-        navbar.style.background = "#fff";
+        navbar.style.background = "transparent";
+        navbar.style.color = "#fff";
       }
 }
